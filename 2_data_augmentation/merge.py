a39b70ac9e8b65dea9e73ec176546dae42155b57
Nikolay Petrov Kormushev <nkormushev@gmail.com>
2024-08-15 14:40:43 +0200

    add time and validations for merge operaion

--- 2_data_augmentation/merge.py
+++ 2_data_augmentation/merge.py
@@ -4,21 +4,27 @@ from dask.distributed import Client
 import sys
 import pandas as pd
 import numpy as np
+import time 
 
 
 base_path = '/d/hpc/projects/FRI/bigdata/students/nk93594/'
 
 if __name__ == '__main__':
+    start = time.time()
     client = Client(
             n_workers=1,
             memory_limit="60GB",
             local_directory="/tmp",
         )
 
+    if len(sys.argv) < 3:
+        print("Usage: python merge_all.py <category> <compression>")
+        sys.exit(1)
+
     pkName = sys.argv[1]
     compression = sys.argv[2]
 
-    pk = dd.read_parquet(base_path + 'dataset_2.parquet')
+    pk = dd.read_parquet(base_path + 'dataset.parquet')
     mergeDf = dd.read_parquet(base_path + f'{pkName}.parquet')
 
     pk['street_code1or2'] = pk['street_code1'].where(pk['street_code1'] != 0, pk['street_code2'].where(pk['street_code2'] != 0, pk['street_code3'])).astype("string")
@@ -31,6 +37,7 @@ if __name__ == '__main__':
         # I do not use street_code because it does not match. We will look at how it affects the whole 
         pk = pk.sample(frac=0.7, random_state=42)
         merged = pk.merge(mergeDf, left_on=['violation_county', 'issue_date'], right_on=['borough', 'date'], how='left')
+
     elif pkName == 'hs':
         merged = pk.merge(mergeDf, left_on=['violation_county', 'street_code1or2', 'DataYear'], right_on=['borough', 'street_code', 'DataYear'], how='left')
 
@@ -43,11 +50,13 @@ if __name__ == '__main__':
         merged = merged.drop(['_date', '_hour'], axis=1)
     else:
         merged = pk.merge(mergeDf, left_on=['violation_county', 'street_code1or2'], right_on=['borough', 'street_code'], how='left')
-
+        
 
     print(f"Saving parquet with compression {compression}")
-    merged.to_parquet(base_path + f'dataset_with_{pkName}.parquet', compression=compression)
+    merged.to_parquet(base_path + f'dataset_with_{pkName}_2.parquet', compression=compression)
 
+    end = time.time()
+    print(f"Execution Time: {end - start}")
 
     print("Closing client")
     client.close()
